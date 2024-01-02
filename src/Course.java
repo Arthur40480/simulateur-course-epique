f4e2d6d37414d8d6f16e49ef886a9876da993219
Arthur Gibert <arthurgibert@free.fr>
2024-01-02 13:23:46 +0100

    Initialisation des variables, affichage des instructions utilisateur et récupération des saisies

--- src/Course.java
+++ src/Course.java
@@ -1,8 +1,29 @@
+import java.util.Scanner;
 
 public class Course {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Déclaration du scanner et des variables qui contiendront le choix de l'utilisateur, ainsi que le nombre de chevaux :
+		Scanner scanner = new Scanner(System.in);
+		int userChoice;
+		int numberHorses;
+		
+		System.out.println("Bonjour, à quel type de course souhaitez-vous jouer ?");
+		System.out.println("1 - Tiercé");
+		System.out.println("2 - Quarté");
+		System.out.println("3 - Quinté");
+		System.out.println("4 - Quittez l'application");
+		
+		// On assigne le choix de l'utilisateur à la variable userChoice :
+		userChoice = scanner.nextInt();
+		
+		System.out.println("Veuillez indiquer le nombre de chevaux dans la course (12 - 20) :");
+		
+		// On assigne le nombre de chevaux à la variable numberHorses :
+		numberHorses = scanner.nextInt();
+		
+		System.out.println("Choix de l'utilisateur :" + userChoice + " - Nombre de chevaux :" + numberHorses);
+		scanner.close();
 
 	}
 
