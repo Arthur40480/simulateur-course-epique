4d6991c3991bac9392fb2ea3757fa5b64875d47d
Arthur Gibert <arthurgibert@free.fr>
2024-01-03 09:09:45 +0100

    Ajout de la méthode raceResult qui permet de créer une liste de numéro par défaut, tout en appelant la méthode contains pour vérifier qu'il n'y ai pas de doublon.

--- src/Finish.java
+++ src/Finish.java
@@ -1,11 +1,34 @@
-import java.util.Random;
+import java.util.Random; 
 
 public class Finish {
 	
-	public static void raceResults() {
+	public static void raceResults(int typeOfRace, int numberHorses) {
+		// La classe Random est utilisé pour générer des nombres aléatoires :
 		Random random = new Random();
-		
-		int result = random.nextInt(18);
-		System.out.println(result);
+		int[] resultatList = new int[typeOfRace];
+		for(int i = 0; i < typeOfRace; i++) {
+			while(true) {
+				int nextHorse = random.nextInt(numberHorses) + 1;
+				System.out.println("Numéro suivant : " + nextHorse);
+				if(contains(resultatList, nextHorse) == false) {
+					resultatList[i] = nextHorse;
+					break;
+				}
+			}
+		}
+		for(int numero : resultatList) {
+			System.out.println(numero + " - ");
+		}
+
+	}
+	
+	// Méthode pour vérifier si un nombre est déjà présent dans le tableau d'arrivé :
+	public static boolean contains(int[] horseNumberList, int nextHorseNumber) {
+		for(int number : horseNumberList) {
+			if(nextHorseNumber == number) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
